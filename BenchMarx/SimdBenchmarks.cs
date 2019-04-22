b19d65461e35cd82f09ed8a00794b72721e2641c
Anton Firszov <antonfir@gmail.com>
2019-04-23 01:01:35 +0200

    basic benchmarks

--- BenchMarx/SimdBenchmarks.cs
+++ BenchMarx/SimdBenchmarks.cs
@@ -1,33 +1,85 @@
+using System;
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using BenchmarkDotNet.Attributes;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 
 namespace BenchMarx
 {
-    [RyuJitX64Job]
-    [RyuJitX86Job]
-    public class SimdBenchmarks
+    internal unsafe struct Buffer : IDisposable
     {
-        private float[] source1;
-        private float[] source2;
-        private float[] dest;
-       
-        [Params(1024)]
-        public int Count { get; set; }
+        public readonly byte[] Array;
 
+        public readonly GCHandle Handle;
+
+        public readonly IntPtr Address;
+
+        public readonly byte* AlignedPtr;
+
+        public readonly byte* UnalignedPtr;
+
+        public Buffer(int count, int alingment)
+        {
+            Array = new byte[count + 2 * alingment];
+            Handle = GCHandle.Alloc(Array, GCHandleType.Pinned);
+            Address = Handle.AddrOfPinnedObject();
+            long addressVal = (long)Address;
+            int padding = alingment - (int)(addressVal % alingment);
+
+            AlignedPtr = (byte*)(Address + padding);
+            UnalignedPtr = AlignedPtr + 3;
+        }
+
+        public void Dispose()
+        {
+            Handle.Free();
+        }
+    }
+    
+    [RyuJitX64Job]
+//    [RyuJitX86Job]
+    public unsafe class SimdBenchmarks
+    {
+        private static readonly int SizeOfVector = Unsafe.SizeOf<Vector<float>>();
+
+        private Buffer _source;
+        private Buffer _dest;
+        
+        private int _byteCount;
+       
+        [Params(512)]
+        public int Count { get; set; }
 
         [GlobalSetup]
         public void Setup()
         {
-            this.source1 = new float[this.Count];
-            this.source2 = new float[this.Count];
-            this.dest = new float[this.Count];
+            _byteCount = Count * SizeOfVector;
+            
+            _source = new Buffer(_byteCount, SizeOfVector);
+            _dest = new Buffer(_byteCount, SizeOfVector);
+        }
+
+        [GlobalCleanup]
+        public void Cleanup()
+        {
+            _source.Dispose();
+            _dest.Dispose();
         }
         
-        [Benchmark]
-        public void Basic()
+        [Benchmark(Baseline = true)]
+        public void ClassicAligned()
         {
-            
+            ref Vector<float> sBase = ref Unsafe.AsRef<Vector<float>>(_source.AlignedPtr);
+            ref Vector<float> dBase = ref Unsafe.AsRef<Vector<float>>(_dest.AlignedPtr);
+
+            for (int i = 0; i < Count; i++)
+            {
+                Vector<float> s = Unsafe.Add(ref sBase, i);
+
+                Unsafe.Add(ref dBase, i) = s * s + s;
+            }
         }
     }
 }
